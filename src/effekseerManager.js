c6b1c29b5b728ea6916858291fcde31573a262e2
MikalDev <danpshimizu@gmail.com>
2024-09-24 15:26:51 -0700

    Initial commit

---
+++ src/effekseerManager.js
@@ -0,0 +1,91 @@
+import effekseer from "./effekseer.js";
+
+class EffekseerManager {
+    constructor() {
+        this.context = null;
+        this.gl = null;
+        this.effects = {};
+        this.handles = {};
+    }
+
+    async initialize(gl, wasmUrl) {
+        // effekseer.setImageCrossOrigin("anonymous");
+        await new Promise((resolve) => {
+            effekseer.initRuntime(wasmUrl, resolve);
+        });
+        this.context = effekseer.createContext();
+        this.context.setRestorationOfStatesFlag(true);
+        this.gl = gl;
+        await this.context.init(gl);
+    }
+
+    async loadEffect(effectUrl) {
+        return await new Promise((resolve, reject) => {
+            let effect;
+            effect = this.context.loadEffect(
+                effectUrl,
+                1.0,
+                () => {
+                    resolve(effect);
+                },
+                reject
+            );
+        });
+    }
+
+    stopEffect(handle) {
+        this.context.stopEffect(handle);
+    }
+
+    setMVPMatrices(MV, P) {
+        if (!this.context) return;
+        this.context.setProjectionMatrix(P);
+        this.context.setCameraMatrix(MV);
+    }
+
+    playEffect(effectName, x, y, z = 0) {
+        if (!this.context) return;
+        const handle = this.context.play(effectName, x, y, z);
+        return handle ? handle : null;
+    }
+
+    update(step) {
+        if (this.context) {
+            this.context.update(step);
+        }
+    }
+
+    setEffectFrame(handle, frame) {
+        if (!handle) return;
+        handle.setFrame(frame);
+    }
+
+    stopEffect(handle) {
+        if (!handle) return;
+        handle.stopEffect();
+    }
+
+    draw() {
+        if (this.context) {
+            this.update(1);
+            this.updateHandles(1);
+            this.context.draw();
+        }
+    }
+
+    updateHandles() {
+        if (!this.context) return;
+        for (const handle in this.handles) {
+            this.context.updateHandle(handle);
+        }
+    }
+
+    release() {
+        if (this.context) {
+            this.context.release();
+            this.context = null;
+        }
+    }
+}
+
+export default EffekseerManager;
---
+++ src/instance.js
@@ -0,0 +1,149 @@
+function getInstanceJs(parentClass, scriptInterface, addonTriggers, C3) {
+    return class extends parentClass {
+        constructor(inst, properties) {
+            super(inst);
+
+            if (properties) {
+                this.scaleX = properties[0];
+                this.scaleY = properties[1];
+                this.scaleZ = properties[2];
+                this.rotationX = (properties[3] * Math.PI) / 180;
+                this.rotationY = (properties[4] * Math.PI) / 180;
+                this.rotationZ = (properties[5] * Math.PI) / 180;
+            }
+
+            this.effect = null;
+            this.handle = null;
+            this.sdkType = inst._objectType._sdkType;
+            this.runtime = this.sdkType.runtime;
+            this.playing = false;
+            this.loaded = false;
+            this.animationSpeed = 1.0;
+            this.uid = inst.GetUID();
+            this.path = null;
+            this._StartTicking();
+        }
+
+        async _LoadEffect(effectPath, name, playEnable = true) {
+            console.log("LoadEffect");
+            const effect = await this.sdkType.effekseerManager.loadEffect(
+                effectPath
+            );
+            this.effect = effect;
+            this.sdkType.effects.set(this.uid, effect);
+            this.loaded = true;
+            this.Trigger(C3.Plugins.mikal_effekseer.Cnds.OnLoaded);
+
+            if (playEnable) {
+                this.handle = this.sdkType.effekseerManager.playEffect(
+                    this.effect
+                );
+                this.sdkType.handles.set(this.uid, this.handle);
+                const wi = this.GetWorldInfo();
+                const x = wi.GetX();
+                const y = wi.GetY();
+                const z = wi.GetTotalZElevation();
+                this.handle.setLocation(x, y, z);
+                this.handle.setScale(this.scaleX, this.scaleY, this.scaleZ);
+                this.handle.setRotation(
+                    this.rotationX,
+                    this.rotationY,
+                    this.rotationZ
+                );
+                // this.playing = true;
+            }
+        }
+
+        _SetScale(scaleX, scaleY, scaleZ) {
+            this.scaleX = scaleX;
+            this.scaleY = scaleY;
+            this.scaleZ = scaleZ;
+        }
+
+        _SetRotation(rotationX, rotationY, rotationZ) {
+            this.rotationX = (rotationX * Math.PI) / 180;
+            this.rotationY = (rotationY * Math.PI) / 180;
+            this.rotationZ = (rotationZ * Math.PI) / 180;
+        }
+
+        _OnLoaded() {
+            return true;
+        }
+
+        _IsLoaded() {
+            return this.loaded;
+        }
+
+        _Play() {
+            if (!this.effect || this.playing) {
+                return;
+            }
+            this.handle = this.sdkType.effekseerManager.playEffect(this.effect);
+            this.sdkType.handles.set(this.uid, this.handle);
+            const wi = this.GetWorldInfo();
+            const x = wi.GetX();
+            const y = wi.GetY();
+            const z = wi.GetTotalZElevation();
+            this.handle.setLocation(x, y, z);
+            this.handle.setScale(this.scaleX, this.scaleY, this.scaleZ);
+            this.handle.setRotation(
+                this.rotationX,
+                this.rotationY,
+                this.rotationZ
+            );
+        }
+
+        _SetFrame(frame) {
+            if (!this.effect || !this.handle) {
+                return;
+            }
+            this.effekseerManager.setEffectFrame(this.handle, frame);
+        }
+
+        _Stop() {
+            if (!this.effect || !this.handle) {
+                return;
+            }
+            this.effekseerManager.stopEffect(this.handle);
+        }
+
+        _Path() {
+            return this.path ? this.path : "";
+        }
+
+        Tick() {
+            const handle = this.handle;
+            if (handle) {
+                const wi = this.GetWorldInfo();
+                const x = wi.GetX();
+                const y = wi.GetY();
+                const z = wi.GetTotalZElevation();
+                handle.setLocation(x, y, z);
+                handle.setRotation(
+                    this.rotationX,
+                    this.rotationY,
+                    this.rotationZ
+                );
+            }
+        }
+
+        async Draw(renderer) {
+            const sdkType = this.sdkType;
+            if (!sdkType.loaded) {
+                await this.sdkType.init(renderer);
+            }
+            const tickCount = this.runtime.GetTickCount();
+            sdkType.Draw(renderer, tickCount);
+        }
+
+        SaveToJson() {
+            return {
+                // data to be saved for savegames
+            };
+        }
+
+        LoadFromJson(o) {
+            // load state for savegames
+        }
+    };
+}
